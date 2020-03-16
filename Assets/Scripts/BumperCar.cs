8b73323202acfb246a3395ab6deb591c8906ab2f
Christopher-Robin <jecebbi@gmail.com>
2020-03-16 12:23:26 +1100

    added Project Files

---
+++ Assets/Scripts/BumperCar.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BumperCar : MonoBehaviour{
+    Rigidbody rb;
+
+    float Direction;
+    
+    public float Speed = 10.0f;
+
+    Vector3 velocity;
+
+    public float Resistance = .1f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        if(rb == null)
+            rb = gameObject.AddComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        
+        velocity = rb.velocity;
+
+        float direction = Input.GetAxis("Horizontal");
+
+        Direction += direction * 0.05f;
+
+        //Direction = Direction;
+
+        Vector2 dirVector = new Vector2(Mathf.Sin(Direction), Mathf.Cos(Direction));
+
+        float speed = Input.GetAxis("Vertical") * Speed;
+
+        Vector2 speedDir = (dirVector * speed) * Time.deltaTime;
+
+        print(speedDir);
+
+        velocity *= (1 - Resistance);
+
+        float rotation = 360f - ((Mathf.Atan2(dirVector.y, dirVector.x) * Mathf.Rad2Deg + 360) % 360);
+
+        rb.rotation = Quaternion.Euler(0, rotation, 0);
+
+        rb.velocity = new Vector3(speedDir.x, rb.velocity.y, speedDir.y);
+    }
+}
---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour{
+    public float Speed = 10.0f;
+
+
+   // Vector2 direction;
+
+    public void Update(){
+        Vector2 InputVector = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        Vector2 direction = InputVector.normalized;
+
+        float rotation = 360 - ((Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg + 360) % 360);
+        print(rotation);
+
+        transform.rotation = Quaternion.Euler(0, rotation, 0);
+        //transform.LookAt(transform.position + new Vector3(direction.x, 0, direction.y));
+        Vector2 finalOffset = direction * Speed * Time.deltaTime;
+        transform.position += new Vector3(finalOffset.x, 0, finalOffset.y);
+    }
+}
