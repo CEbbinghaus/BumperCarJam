d5cd6689ebcedbdcac6a1aaae4455709bfe4e06a
GameJam Alex <alex.mollard@mail.com>
2020-03-17 16:00:16 +1100

    Added stuff

--- Assets/Scripts/BoostBar.cs
+++ Assets/Scripts/BoostBar.cs
@@ -8,6 +8,7 @@ public class BoostBar : MonoBehaviour
     // 1.0f Means full charge.
     public float BoostAmount = 0.25f;
     public Slider[] boostBar = null;
+    public Slider[] healthBar = null;
     public OldPlayer[] Player = null;
     // Start is called before the first frame update
     void Start()
@@ -21,6 +22,7 @@ public class BoostBar : MonoBehaviour
         for (int i = 0; i < Player.Length; i++)
         {
             boostBar[i].value = Player[i].BoostAmount;
+            healthBar[i].value = Player[i].healthAmount;
         }
     }
 }
